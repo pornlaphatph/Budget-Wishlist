b2611a9bb95cfe3ce9929eb554ffacf2a1c57ef7
pornlaphatph <pornlaphat.ph@ku.th>
2026-03-28 12:26:06 +0700

    up

--- demo/src/main/java/com/project/App.java
+++ demo/src/main/java/com/project/App.java
@@ -8,12 +8,12 @@ public class App {
        
     SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                Budget app = new Budget();
-                app.setTitle("YET?");
-                app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                app.setSize(1000, 700);
-                app.setLocationRelativeTo(null);
-                app.setVisible(true);
+                Budget budget = new Budget();
+                budget.setTitle("YET?");
+                budget.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                budget.setSize(1000, 700);
+                budget.setLocationRelativeTo(null);
+                budget.setVisible(true);
             }
         });     
     }
